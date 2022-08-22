de53a323ac341c1b2626c325cebabfc2cb6f2e0b
ffazulov <102616534+ffazulov@users.noreply.github.com>
2022-08-23 01:02:57 +0500

    Create 36.py

---
+++ 36.py
@@ -0,0 +1,13 @@
+from math import sqrt
+from math import ceil
+ 
+a = int(input())
+c = 0
+for t in range(a + 1, 2 * a):
+    c += 1
+    for j in range(2, ceil(sqrt(t)) + 1):
+        if t % j == 0:
+            c -= 1
+            break
+ 
+print(c)
