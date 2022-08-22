a63f7ef49eef43c31b413aee2592f3efb47a1300
ffazulov <102616534+ffazulov@users.noreply.github.com>
2022-08-23 01:01:32 +0500

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,35 @@
+def main():
+    def all_perms(elements):
+        if len(elements) <= 1:
+            yield elements
+        else:
+            for perm in all_perms(elements[1:]):
+                for i in range(len(elements)):
+                    yield perm[:i] + elements[0:1] + perm[i:]
+ 
+    def counter(r):
+        c = 0
+        for i in range(len(r)):
+            if r[i] == i + 1:
+                c += 1
+        return 1 if c == k else 0
+ 
+    with open("input.txt", "r") as f:
+        n, k = list(map(int, f.readline().split()))
+ 
+    # total = 0
+    match = 0
+    a = [*range(1, n + 1)]
+    for p in all_perms(a):
+        # total += 1
+        match += counter(p)
+ 
+    # print('total=', total)
+    # print('match=', match)
+ 
+    with open("output.txt", "w") as g:
+        g.write(str(match))
+ 
+ 
+if __name__ == "__main__":
+    main()
