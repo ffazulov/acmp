5dddff6ff29e4ac50ea1e24fc0f0cfa489fd464f
ffazulov <102616534+ffazulov@users.noreply.github.com>
2022-08-26 00:36:23 +0500

    Create 892.py

---
+++ 851-900/892.py
@@ -0,0 +1,11 @@
+x = int(input())
+if x <=2 or x==12:
+  print('Winter')
+elif 3<=x<=5:
+  print('Spring')
+elif 6<=x<=8:
+  print('Summer')
+elif 9<=x<=11:
+  print('Autumn')
+else:
+  print('Error')
